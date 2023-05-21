04c766e7b19e119ff0ace75f708c109dee186a11
Breno RdV <noreply@raccoon.ninja>
2023-05-21 03:18:03 -0400

    Adding a workaround since Docker can't access folders outside the build directory

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# Copy app folder to this folder, since docker can't access parent directories.
+cp -r ../app .
+
 docker build -t raccoon_conn_monitor:latest .
+
+# Remove local app folder
+rm -rf app
